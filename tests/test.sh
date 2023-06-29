3663739fc8fc3ae2b0b809b00310c79dc3893055
Derek Pepple <dpepple@nd.edu>
2023-06-29 09:49:49 -0400

    Testing

--- tests/test.sh
+++ tests/test.sh
@@ -1,16 +1,16 @@
 # /bin/sh
 
-t=2
+t=1
 
 if [ $# -ge 2 ]; then
 
-    while [ $t -lt 1025 ]
+    while [ $t -lt 33 ]
     do
         ns=$($1 -t $t -m 2>&1 > /dev/null);
         echo "$t,$ns" >> $2;
      
         echo "Finished test with $t threads"
-        t=$(($t * 2));
+        t=$(($t + 1));
     done
 
 else
