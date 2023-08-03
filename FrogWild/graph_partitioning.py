f3dd73bd5c42c0e4c20dbec4a3eb514269c47667
jvap2 <jayvap6181@gmail.com>
2023-08-03 09:06:38 -0400

    Further ALterations

--- FrogWild/graph_partitioning.py
+++ FrogWild/graph_partitioning.py
@@ -1,9 +1,12 @@
 import cugraph
 import cudf
 
+num_clusters=10
 graph= cudf.read_csv("webGoogle.csv", delimiter='\t', dtype=['int32', 'int32'])
 print(graph.head())
 g=cugraph.Graph()
 g.from_cudf_edgelist(graph, source='FromNodeId', destination='ToNodeId')
-df = cugraph.spectralBalancedCutClustering(g, 5)
-print(df)
+df = cugraph.spectralBalancedCutClustering(g, num_clusters=num_clusters, num_eigen_vects=num_clusters-2)
+print(cugraph.analyzeClustering_edge_cut(g,num_clusters,df))
+df.to_csv("Cluster_Assignment.csv")
+
